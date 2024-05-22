cc5b42a44776e98d391351de99b4afcfe52175ab
Eric Hiller <eric@hiller.pro>
2024-05-22 17:34:08 +0300

    Update grunt.tasks.js

--- grunt.tasks.js
+++ grunt.tasks.js
@@ -20,7 +20,7 @@ module.exports = function (grunt) {
         'copy:icons',
         // 'copy:wallpapers',
         'copy:manifest',
-        'htmlinkrel:images',
+        // 'htmlinkrel:images',
         'webpack:app',
         'inline',
         'htmlinkrel:assets',
