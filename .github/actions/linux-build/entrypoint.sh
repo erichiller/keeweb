507ced1a851cda1a7dd4ba9d89682ef3bf680637
Eric Hiller <eric@hiller.pro>
2024-05-23 00:38:42 +0300

    Update entrypoint.sh

--- .github/actions/linux-build/entrypoint.sh
+++ .github/actions/linux-build/entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/bash -ex
 
 echo "Beginning ($0)"
+
+apt install snapd
+snap install snapcraft --classic
 git config --global --add safe.directory /github/workspace
 
 echo "-- Installing packages --"
