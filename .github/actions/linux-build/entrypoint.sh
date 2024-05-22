60bed2964e8a862a1a26f05fb5bf7702ad433737
Eric Hiller <eric@hiller.pro>
2024-05-22 22:55:26 +0300

    Update entrypoint.sh

--- .github/actions/linux-build/entrypoint.sh
+++ .github/actions/linux-build/entrypoint.sh
@@ -1,6 +1,10 @@
-#!/bin/bash -e
+#!/bin/bash -e -x
 
-
+echo "Beginning ($0)"
 git config --global --add safe.directory /github/workspace
+
+echo "-- Installing packages --"
 npm ci
+
+echo "-- grunt desktop-linux --"
 grunt desktop-linux
